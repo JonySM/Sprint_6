c7da7ba74f1eee976ccbc3e8472271146332deb4
Evgeniy Sergeev <jony1922sm@yandex.ru>
2024-08-21 21:49:13 +0300

    Fixed_fixture

--- conftest.py
+++ conftest.py
@@ -1,17 +1,15 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from settings import Url
 
 
 @pytest.fixture(scope='function')
 def driver():
-    firefox_driver = webdriver.Firefox(service=FirefoxService(
-    r'C:\Users\e.sergeev\WebDriver\bin\geckodriver.exe'))
-    firefox_driver.maximize_window()
-    firefox_driver.get(Url.QA_SCOOTER_SERVICE)
-    yield firefox_driver
-    firefox_driver.quit()
+    firefox = webdriver.Firefox()
+    firefox.maximize_window()
+
+    yield firefox
+
+    firefox.quit()
 
 
 
